d643adf657c57e419292dd3150c4a93a1cb6f96c
MK-Khan123 <mehnazkhan231@gmail.com>
2022-06-15 20:52:58 +0600

    Added DELETE API for deleting reading materials

--- index.js
+++ index.js
@@ -153,6 +153,13 @@ async function run() {
             res.json(result);
         });
 
+        app.delete('/deleteReadingMaterials/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await readingMaterialsCollection.deleteOne(query);
+            res.json(result);
+        });
+
 
     } finally {
         // await client.close();
